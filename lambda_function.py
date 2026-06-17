58249869d9f878df08b6d9b34a7dee45c5c1c0b7
Akhil merugu <93387094+akhilgoud8@users.noreply.github.com>
2026-06-17 13:23:58 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,55 +1,96 @@
 import json
-import os
 import boto3
-
+from urllib.parse import parse_qs
 def lambda_handler(event, context):
     try:
-        mypage = page_router(event['httpMethod'], event['queryStringParameters'], event['body'])
-        return mypage
+        print("Received Event:")
+        print(json.dumps(event))
+
+        # Get HTTP method safely
+        httpmethod = event.get('httpMethod')
+
+        if not httpmethod:
+            httpmethod = (
+                event.get('requestContext', {})
+                     .get('http', {})
+                     .get('method')
+            )
+
+        if not httpmethod:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({
+                    "error": "No HTTP method found in event"
+                })
+            }
+
+        querystring = event.get('queryStringParameters')
+        formbody = event.get('body')
+
+        return page_router(httpmethod, querystring, formbody)
+
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': json.dumps({
+                'error': str(e)
+            })
         }
 
+
 def page_router(httpmethod, querystring, formbody):
+
     if httpmethod == 'GET':
-        try:
-            with open('index.html', 'r') as htmlFile:
-                htmlContent = htmlFile.read()
-            return {
-                'statusCode': 200,
-                'headers': {"Content-Type": "text/html"},
-                'body': htmlContent
-            }
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': str(e)})
-            }
+
+        with open('index.html', 'r') as htmlFile:
+            htmlContent = htmlFile.read()
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/html'
+            },
+            'body': htmlContent
+        }
 
     elif httpmethod == 'POST':
-        try:
-            insert_record(formbody)
-            with open('success.html', 'r') as htmlFile:
-                htmlContent = htmlFile.read()
-            return {
-                'statusCode': 200,
-                'headers': {"Content-Type": "text/html"},
-                'body': htmlContent
-            }
-        except Exception as e:
-            return {
-                'statusCode': 500,
-                'body': json.dumps({'error': str(e)})
-            }
+
+        insert_record(formbody)
+
+        with open('success.html', 'r') as htmlFile:
+            htmlContent = htmlFile.read()
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/html'
+            },
+            'body': htmlContent
+        }
+
+    else:
+        return {
+            'statusCode': 405,
+            'body': json.dumps({
+                'error': f'Method {httpmethod} not allowed'
+            })
+        }
+
 
 def insert_record(formbody):
-    formbody = formbody.replace("=", "' : '")
-    formbody = formbody.replace("&", "', '")
-    formbody = "INSERT INTO veera value {'" + formbody + "'}"
 
-    client = boto3.client('dynamodb')
-    response = client.execute_statement(Statement=formbody)
-    # Assuming the execute_statement call returns successfully
-    return response
+    data = parse_qs(formbody)
+
+    table = boto3.resource('dynamodb').Table('mydb')
+
+    table.put_item(
+        Item={
+            'mykey': data['email'][0],      # Primary Key
+            'fname': data['fname'][0],
+            'lname': data['lname'][0],
+            'email': data['email'][0],
+            'message': data['message'][0]
+        }
+    )
+
+    return True
